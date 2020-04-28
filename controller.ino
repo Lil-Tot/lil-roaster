1d67e893a43442c9b1abfe26b4d97f3a99ce1e24
Lil-Tot <sventura137@gmail.com>
2020-04-27 23:01:13 -0400

    Added simple control for heating based off temp
    - Utilized a transitor as a switch
    - png file of simple control output in Artisan

--- controller.ino
+++ controller.ino
@@ -1,7 +1,11 @@
 #include <Adafruit_MAX31856.h>
 // Use SPI pins: CS, MISO, MOSI, CLK
-Adafruit_MAX31856 tc0 = Adafruit_MAX31856(D0,D7,D6,D5);
+Adafruit_MAX31856 tc0 = Adafruit_MAX31856(D2,D7,D6,D5);
 Adafruit_MAX31856 tc1 = Adafruit_MAX31856(D1,D7,D6,D5);
+
+
+uint8_t heating_elements = D4;
+int temp_setting = 300;
 
 void setup() {
   Serial.begin(115200);
@@ -9,10 +13,18 @@ void setup() {
   tc1.begin();
   tc0.setThermocoupleType(MAX31856_TCTYPE_K);
   tc1.setThermocoupleType(MAX31856_TCTYPE_K);
+  pinMode(heating_elements, OUTPUT);
 }
 
 void loop() {
+
   float temp0 = (tc0.readThermocoupleTemperature() * 9.0/5.0) + 32.0;
   float temp1 = (tc1.readThermocoupleTemperature() * 9.0/5.0) + 32.0;
   Serial.print(temp0); Serial.print(","); Serial.println(temp1);
+
+  // Simple temperature control
+  if(temp0 < temp_setting)
+    digitalWrite(heating_elements, HIGH);
+  else
+    digitalWrite(heating_elements, LOW);
 }
